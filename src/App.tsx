09f5ff71924861a40b669a182b22be35bd6e923f
kareem muhammad <kareem.gaad@gmail.com>
2021-11-29 17:57:53 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-
-import gacc from "../src/assets/gacc.png";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Landing from "./pages/Landing/Landing";
 import Footer from "./components/Footer/Footer";
@@ -14,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/home" component={Home} />
+          <Redirect to="/" />
         </Switch>
       </div>
       <Footer />
